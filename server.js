fe2101ed6689ec604120db05fde4737cc9868325
Dominic Fuentes <militaryforce1@yahoo.com>
2023-02-10 16:00:39 -0600

    react app created

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const routes = require('./routes');
 const db = require('./db');
 const logger = require('morgan');
 const cors = require('cors')
-// require() imports and middleware here ^ ///////
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,7 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(logger('dev'))
-// app.use() middleware here ^ ///////////////////
 
 app.use('/api', routes);
 
