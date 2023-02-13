8caa8ba02cbd5228190732e4708f34258435c7f6
Dominic Fuentes <militaryforce1@yahoo.com>
2023-02-13 13:41:38 -0600

    add images to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,14 +4,16 @@ const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <h2>NFC</h2>
             <Link to={"/division/nfc"}>
             <div>
-                <h1>NFC</h1>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/American_Football_Conference_logo.svg/1200px-American_Football_Conference_logo.svg.png' alt=''></img>
             </div>
             </Link>
             <div>
+            <h2>AFC</h2>
             <Link to={"/division/afc"}>
-                <h1>AFC</h1>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/National_Football_Conference_logo.svg/1200px-National_Football_Conference_logo.svg.png' alt=''></img>
             </Link>
             </div>
             
