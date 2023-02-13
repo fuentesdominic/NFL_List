293d8bc26b329d0aed85d70ccbea3d0b1174f31f
Dominic Fuentes <militaryforce1@yahoo.com>
2023-02-13 16:38:53 -0600

    create for crud is done

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -19,7 +19,13 @@ const Team = ({allTeams, allPlayers}) => {
 
     const handleSubmit = async (evt) => {
         evt.preventDefault()
-        await axios
+        await axios.post('/teams/players', createPlayers)
+        setCreatePlayers(addPlayers)
+        allPlayers()
+    }
+
+    const handleChange = (evt) => {
+        setCreatePlayers({...createPlayers, [evt.target.id]: evt.target.value})
     }
 
     return (
@@ -40,8 +46,37 @@ const Team = ({allTeams, allPlayers}) => {
                     <p>{player.position}</p>
                 </div>
             ))}
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Name:</label>
+                <textarea
+                    id="name"
+                    cols='30'
+                    rows='1'
+                    onChange={handleChange}
+                    value={createPlayers.name}></textarea>
+                <label htmlFor="age">Age:</label>
+                <textarea
+                    id="age"
+                    cols='2'
+                    rows='1'
+                    onChange={handleChange}
+                    value={createPlayers.age}></textarea>
+                <label htmlFor="position">Position</label>
+                <select id="position" onChange={handleChange} value={createPlayers.position}>
+                    <option value='quarterback'>Quarterback</option>
+                    <option value='offensive linemen'>Offensive Linemen</option>
+                    <option value='running back'>Running Back</option>
+                    <option value='tight end'>Tight End</option>
+                    <option value='wide receiver'>Wide Receiver</option>
+                    <option value='defensive linemen'>Defensive Linemen</option>
+                    <option value='linebacker'>Linebacker</option>
+                    <option value='cornerback'>Cornerback</option>
+                    <option value='safety'>Safety</option>
+                </select>
+                <button type='submit'>Add Player</button>
+            </form>
         </div>
-
+        
     )
 }
 // .fitler in order to grab each player.
