aaabbc8a8bff0dc58c983e1a878f8e33821a1bac
Dominic Fuentes <militaryforce1@yahoo.com>
2023-07-14 19:53:17 -0500

    clean up

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,6 @@ const Header = () => {
       <header>
         <nav>
             <Link className='homelink' to='/'>Home</Link>
-            {/* <Link className='create' to='/create'>Add Player</Link> */}
             <img className='nflLogo' src='https://static.www.nfl.com/image/upload/v1554321393/league/nvfr7ogywskqrfaiu38m.svg' alt=''></img>
         </nav>
       </header>
--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -1,37 +1,31 @@
 import { useNavigate, useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 import axios from "axios"
-import CreatePlayer from "../components/CreatePlayer"
 
 const Team = ({allTeams, allPlayers, getAllPlayers}) => {
     
     const {team} = useParams()
     const teams = allTeams.filter(player => player.team === team)
 
-    const createPlayerNav = useNavigate('/create');
+    const createPlayerNav = useNavigate();
 
-    // const addPlayer = {
-    //     name: '',
-    //     age: '',
-    //     position: '',
-    //     team: `${team}`
-    // }
-
-    // const [createPlayer, setCreatePlayer] = useState(addPlayer)
     const [currentPlayers, setCurrentPlayers] = useState()
 
     const [positions, setPositions] = useState({position: ''})
-    const [showEdit, setShowEdit] = useState(false)
+    const [showEdit, setShowEdit] = useState('')
 
     const handlePut = async (id) => {
         console.log(id, positions[id])
         const res = await axios.put(`http://localhost:3001/api/teams/players/${id}`, {position: positions[id]})
         getAtPlayers()
     }
+
+ 
+
     const handleEdit = (evt) => {
         setPositions({[evt.target.id]: evt.target.value})
-        
     }
+      
     const handleShowEdit = () => {
         setShowEdit(!showEdit)
     }
@@ -53,17 +47,6 @@ const Team = ({allTeams, allPlayers, getAllPlayers}) => {
     const handleCreateNav = () => {
         createPlayerNav(`/create/${team}`)
     };
-
-    // const handleSubmit = async (evt) => {
-    //     evt.preventDefault()
-    //     await axios.post('/api/teams/players', createPlayer)
-    //     setCreatePlayer(addPlayer)
-    //     getAllPlayers()
-    // }
-
-    // const handleChange = (evt) => {
-    //     setCreatePlayer({...createPlayer, [evt.target.id]: evt.target.value})
-    // }
 
     const handleDelete = async (id) => {
         await axios.delete(`http://localhost:3001/api/teams/players/${id}`)
@@ -94,52 +77,27 @@ const Team = ({allTeams, allPlayers, getAllPlayers}) => {
                     <h4>{player.name}</h4>
                     <p>{player.age}</p>
                     <p>{player.position}</p>
-                    <button onClick={handleShowEdit}>Edit</button>
-                    <button onClick={() => handleDelete(player._id)}>Delete</button>
-                    {showEdit && <div> <label htmlFor={player._id}>Position:</label> <select id={player._id} onChange={handleEdit} value={positions.position}>
-                    <option value='Quarterback'>Quarterback</option>
-                    <option value='Offensive Linemen'>Offensive Linemen</option>
-                    <option value='Running Back'>Running Back</option>
-                    <option value='Tight End'>Tight End</option>
-                    <option value='Wide Receiver'>Wide Receiver</option>
-                    <option value='Defensive Linemen'>Defensive Linemen</option>
-                    <option value='Linebacker'>Linebacker</option>
-                    <option value='Cornerback'>Cornerback</option>
-                    <option value='Safety'>Safety</option>
+                <button onClick={handleShowEdit}>Edit</button>
+                <button onClick={() => handleDelete(player._id)}>Delete</button>
+                    {showEdit && <div> <label htmlFor={player._id}>Position:</label> <select 
+                    id={player._id} 
+                    onChange={(evt) => handleEdit(evt, player._id)} 
+                    value={positions.position}>
+                        <option value='' selected disabled>Select</option>
+                        <option value='Quarterback'>Quarterback</option>
+                        <option value='Offensive Linemen'>Offensive Linemen</option>
+                        <option value='Running Back'>Running Back</option>
+                        <option value='Tight End'>Tight End</option>
+                        <option value='Wide Receiver'>Wide Receiver</option>
+                        <option value='Defensive Linemen'>Defensive Linemen</option>
+                        <option value='Linebacker'>Linebacker</option>
+                        <option value='Cornerback'>Cornerback</option>
+                        <option value='Safety'>Safety</option>
                 </select>
                 <button onClick={() => handlePut(player._id)}>Confirm</button> </div>
                 }
                 </div>
             ))}
-            {/* <form className="createPlayer" onSubmit={handleSubmit}>
-            <label className="createName" htmlFor="name">Name:</label>
-                <textarea
-                    id="name"
-                    cols='30'
-                    rows='1'
-                    onChange={handleChange}
-                    value={createPlayer.name}></textarea>
-                <label className="createAge" htmlFor="age">Age:</label>
-                <textarea
-                    id="age"
-                    cols='2'
-                    rows='1'
-                    onChange={handleChange}
-                    value={createPlayer.age}></textarea>
-                <label className="createSelection" htmlFor="position">Position:</label>
-                <select id="position" onChange={handleChange} value={createPlayer.position}>
-                    <option value='Quarterback'>Quarterback</option>
-                    <option value='Offensive Linemen'>Offensive Linemen</option>
-                    <option value='Running Back'>Running Back</option>
-                    <option value='Tight End'>Tight End</option>
-                    <option value='Wide Receiver'>Wide Receiver</option>
-                    <option value='Defensive Linemen'>Defensive Linemen</option>
-                    <option value='Linebacker'>Linebacker</option>
-                    <option value='Cornerback'>Cornerback</option>
-                    <option value='Safety'>Safety</option>
-                </select>
-                <button className="addPlayer" type='submit'>Add Player</button>
-            </form> */}
             
             <button onClick={handleCreateNav}>Create New Player!</button>
 
